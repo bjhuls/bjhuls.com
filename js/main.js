2648cc2b0f2ecd9625c6ec1f6271b9a3b5949200
williamhuls <71887417+williamhuls@users.noreply.github.com>
2022-11-26 13:22:40 -0600

    added css single page html and js

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    let currentPage = "home";
+    let currentNavLink = $("#nav-ul").children().first().children()[0];
+
+    $("#portfolio").hide();
+    $("#about").hide();
+    $("#contact").hide();
+
+    $(".nav-link").click(function (e) {
+        $(`#${currentPage}`).fadeOut(100)
+        $(`#${e.currentTarget.innerHTML}`).fadeIn(500)
+
+        currentPage = e.currentTarget.innerHTML
+
+        $(currentNavLink).removeClass("selected");
+        $(this).addClass("selected");
+
+        currentNavLink = this;
+    })
+
+    let characterIndex = 0;
+
+    let titleNames = ["b", "bj", "bjh", "bj", "b", ""];
+
+    window.updateTitleName = function () {
+        document.getElementsByTagName("title")[0].innerHTML = titleNames[characterIndex++ % titleNames.length];
+    }
+
+    setInterval(() => {
+        window.updateTitleName()
+    }, 500);
+
+})
